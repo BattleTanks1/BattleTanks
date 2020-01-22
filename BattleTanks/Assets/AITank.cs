b213010e4473441af667bb6da1ae411a8f62ae41
Ryan Swann <RyanSwann1992@gmail.com>
2020-01-22 00:51:52 +0000

    Now using virtual functions

--- BattleTanks/Assets/AITank.cs
+++ BattleTanks/Assets/AITank.cs
@@ -9,7 +9,7 @@ public enum eAIState
 {
     FindingEnemy = 0,
     MovingToSafety,
-    Attack,
+    Shoot,
     SetDestinationToSafePosition,
     Idling
 }
@@ -18,56 +18,47 @@ public class AITank : Tank
 {
     [SerializeField]
     private eAIState m_currentState;
-    [SerializeField]
-    private int m_currentTargetID;
 
     // Start is called before the first frame update
-    void Start()
+    protected override void Start()
     {
-        m_currentTargetID = -1;
+        base.Start();
+
         m_ID = GameManager.Instance.addTank(this);
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update() 
     {
+        base.Update();
+
         switch(m_currentState)
         {
             case eAIState.FindingEnemy:
+                if (isInRange(GameManager.Instance.m_player.transform.position))
                 {
-                    foreach (AITank otherTank in GameManager.Instance.m_tanks)
-                    {
-                        if (m_ID != otherTank.m_ID &&
-                            Vector3.Distance(transform.position, otherTank.transform.position) <= Mathf.Abs(m_minDistance))
-                        {
-                            m_currentTargetID = otherTank.m_ID;   
-                            m_currentState = eAIState.Attack;
-                            break;
-                        }
-                    }
+                    print("Shoot");
+                    m_currentState = eAIState.Shoot;
+                    
                 }
                 break;
+
             case eAIState.SetDestinationToSafePosition:
                 m_velocity = -m_velocity;
                 m_currentState = eAIState.MovingToSafety;
                 break;
+
             case eAIState.MovingToSafety:
                 break;
-            case eAIState.Attack:
-                {
-                    AITank target = GameManager.Instance.GetTank(m_currentTargetID);
-                    if (target)
-                    {
-                        if (Vector3.Distance(target.transform.position, transform.position) <= Mathf.Abs(m_minDistance))
-                        {
-                            shoot();
-                            m_currentState = eAIState.SetDestinationToSafePosition;
-                        }
-                    }
-                }
+
+            case eAIState.Shoot:
+                shoot();
                 break;
+
             case eAIState.Idling:
                 break;
         }
+
+        transform.position += m_velocity * Time.deltaTime;
     }
 }
