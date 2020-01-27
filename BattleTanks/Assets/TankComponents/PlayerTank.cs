b1107b58bf3a8f4385cbfa772050daf31d030bb3
PheroTyrian <32231688+PheroTyrian@users.noreply.github.com>
2020-01-27 16:34:10 +0000

    mouse to world position code finished pending testing

--- BattleTanks/Assets/TankComponents/PlayerTank.cs
+++ BattleTanks/Assets/TankComponents/PlayerTank.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class PlayerTank : MonoBehaviour
 {
+    Vector3 linePlaneIntersection(Vector3 linePos, Vector3 lineDir, Vector3 planePos, Vector3 planeNormal)
+    {
+        float lineDotNormal = Vector3.Dot(lineDir, planeNormal);
+        if (lineDotNormal == 0)
+            return new Vector3();
+
+        float d = Vector3.Dot((planePos - linePos), planeNormal) / lineDotNormal;
+        return linePos + (lineDir * d);
+    }
+
+    Vector3 mousePosToPlanePos(Vector3 worldPos, Vector3 planeNormal, Vector3 planePos)
+    {
+        Camera cam = Camera.main;
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        Vector3 pointA = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        Vector3 eyeToA = pointA - cam.transform.position;
+        Vector3 mouseWorldPos = linePlaneIntersection(cam.transform.position, eyeToA, planePos, planeNormal);
+
+        Vector3 vecBetween = worldPos - mouseWorldPos;
+        return vecBetween;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
