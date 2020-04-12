0afd197088a6a034d4d89587a08b9e708d077a7b
Ryan Swann <RyanSwann1992@gmail.com>
2020-04-12 20:35:17 +0100

    Removed Rectangle

--- BattleTanks/Assets/Utilities.cs
+++ BattleTanks/Assets/Utilities.cs
@@ -2,44 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rectangle
-{
-    public Rectangle(int left, int right, int top, int bottom)
-    {
-        m_left = left;
-        m_right = right;
-        m_top = top;
-        m_bottom = bottom;
-    }
 
-    public Rectangle(Vector2Int position, int distance)
-    {
-        reset(position, distance);
-    }
-
-    public void reset(Vector2Int position, int distance)
-    {
-        Vector2Int mapSize = fGameManager.Instance.m_mapSize;
-
-        m_left = Mathf.Max(position.x - distance, 0);
-        m_right = Mathf.Min(position.x + distance, mapSize.x);
-        if (m_right == mapSize.x)
-        {
-            --m_right;
-        }
-        m_top = Mathf.Max(position.y - distance, 0);
-        m_bottom = Mathf.Min(position.y + distance, mapSize.y);
-        if (m_bottom == mapSize.y)
-        {
-            --m_bottom;
-        }
-    }
-
-    public int m_left { get; private set; }
-    public int m_right { get; private set; }
-    public int m_top { get; private set; }
-    public int m_bottom { get; private set; }
-}
 
 public enum eDirection2D
 {
