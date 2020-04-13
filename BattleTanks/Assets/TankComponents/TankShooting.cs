e2a5a9d727eb873d6c15a0473b446e3b175821c6
Ryan Swann <RyanSwann1992@gmail.com>
2020-04-13 22:39:34 +0100

    Tank Shooting implemented

---
+++ BattleTanks/Assets/TankComponents/TankShooting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShooting : MonoBehaviour
+{
+    public float m_shootRange;
+    [SerializeField]
+    protected Rigidbody m_projectile = null;
+    [SerializeField]
+    private float m_projectileSpeed = 0.0f;
+
+    public float m_timeBetweenShot;
+    private float m_elaspedTime = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireAtPosition(Vector3 position)
+    {
+        //If enemy in range
+        if (m_elaspedTime >= m_timeBetweenShot &&
+            Vector3.Distance(position, transform.position) <= m_shootRange)
+        {
+            m_elaspedTime = 0.0f;
+
+            Rigidbody clone;
+            clone = Instantiate(m_projectile, transform.position, Quaternion.identity);
+            Vector3 vBetween = position - transform.position;
+            clone.velocity = transform.TransformDirection(vBetween.normalized * m_projectileSpeed);
+        }
+    }
+}
