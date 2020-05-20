d130156c2ce1e244c3cb2e708c24e545c8467f50
PheroTyrian <trislancaster@gmail.com>
2020-05-20 03:04:14 +0100

    Game can be exited

--- BattleTanks/Assets/CameraController.cs
+++ BattleTanks/Assets/CameraController.cs
@@ -40,6 +40,11 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (m_movableByMouse)
         {
             MoveByMouse();
